454132cc329157d3de72c99736678f64d69b8d8f
Kaific Maliqon <iamkaif368@gmail.com>
2026-03-11 11:04:44 +0530

    Add promise example with success and error handling

---
+++ Lab MST/promise.js
@@ -0,0 +1,13 @@
+let myPromise = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        resolve("Data fetched successfully");
+    }, 2000);
+});
+
+myPromise
+.then(function(message) {
+    console.log(message);
+})
+.catch(function(error) {
+    console.log(error);
+});
